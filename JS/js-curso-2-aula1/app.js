0fdb58e06df31e31928ce1333aaa0d27c5809f7d
PAULO HENRIQUE VIDAL <vidal081089@gmail.com>
2025-05-17 12:20:01 -0300

    segundo commit, seguindo com as aulas, manipulando HTML via JS

---
+++ JS/js-curso-2-aula1/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector("h1");
+titulo.innerHTML = "Jogo do número seecreto.";
+
+let paragrafo = document.querySelector("p");
+paragrafo.innerHTML = "Escolha um númeor entre 1 e 10.";
+
+function verificarChute(params) {
+  console.log("O botão foi clicado");
+}
